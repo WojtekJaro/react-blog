a1fc123f150dc4218cb3b2c31b0d61c1b79c9d93
Dawid <dawtom97@gmail.com>
2023-07-05 12:35:57 +0200

    simple css post refactor

---
+++ src/components/Post/Post.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Post.css"
+
+const Post = ({item}) => {
+  return (
+    <div className="post">
+      <img
+        src="https://cdn.pixabay.com/photo/2023/06/28/12/43/flower-8094368_1280.jpg"
+        alt=""
+      />
+      <div className="text">
+        <h2>Sportowe podsumowanie 2022 – kto jest najlepszym sportowcem?</h2>
+        <h6>KUBA 22 STYCZNIA, 2023</h6>
+        <p>
+          Ranking to wyjątkowa rzecz. Warto poznać go i zainteresować się kto
+          jest najlepszym sportowcem. W 2022 roku co najmniej kilku sportowców
+          było bardzo ważnych....
+        </p>
+        <button>Więcej</button>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
