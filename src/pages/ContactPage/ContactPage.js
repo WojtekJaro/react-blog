47985841e816dd65082c17026a8398c20eeb9e03
Dawid <dawtom97@gmail.com>
2023-07-05 10:27:11 +0200

    contact page bugfix

--- src/pages/ContactPage/ContactPage.js
+++ src/pages/ContactPage/ContactPage.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <div>
+        <h2>Contact</h2>
+        <span>Phone: 567-654-456</span>
+    </div>
   )
 }
 
